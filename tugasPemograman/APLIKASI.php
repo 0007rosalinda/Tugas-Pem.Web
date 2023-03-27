311fe109accf15fb709ca0d182e30799fdfcdaf0
0007rosalinda <127834528+0007rosalinda@users.noreply.github.com>
2023-03-27 17:01:47 +0700

    Add files via upload

---
+++ tugasPemograman/APLIKASI.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>PENJUMLAHAN, PENGURANGAN, PERKALIAN DAN PEMBAGIAN</title>
+</head>
+
+<body>
+    <h5> ROSALINDA GADISELI SIHOMBING </h5>
+    <h5> 221510007 </h5>
+    <h5> OPERASI ARITMATIKA </h5>
+    <form action="pert3.php" method="POST">
+        Bilangan 1 = <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 = <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">TAMBAH</button>
+    </form>
+   
+</body>
+</html>
